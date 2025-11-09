3cdd5193f6b82f0318c8e9ff79002fb4b5560fa0
Frederico Limberger <fredericoal@gmail.com>
2025-11-09 13:49:26 -0300

    safety codes

--- src/firebase.ts
+++ src/firebase.ts
@@ -4,6 +4,8 @@ import { getFirestore, collection, getDocs, addDoc } from 'firebase/firestore';
 import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
+console.log('API Key:', import.meta.env.VITE_FIREBASE_API_KEY)
+
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -16,6 +18,7 @@ const firebaseConfig = {
     appId: "1:629960583168:web:55bb06840d92a3414e10e5",
     measurementId: "G-6T8P6Y3VC4"
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
